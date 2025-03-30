258b6cf7ae7a93e3c1f0b36c5cd89cb8f55fdd02
Angelo Dureghello <angelo@kernel-space.org>
2025-03-30 11:18:20 +0200

    update command script
    Signed-off-by: Angelo Dureghello <angelo@kernel-space.org>

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -5,52 +5,52 @@
 
 source /etc/sysghost/lib.sh
 
-setup_backlight() {
-	chown root:video /sys/class/backlight/nv_backlight/brightness
-	chown root:video /sys/class/backlight/nv_backlight/max_brightness
-	chmod g+w /sys/class/backlight/nv_backlight/brightness
-	chmod g+r /sys/class/backlight/nv_backlight/max_brightness
-	cat /sys/class/backlight/nv_backlight/max_brightness > \
-		/sys/class/backlight/nv_backlight/brightness
-}
-
-net_wait() {
-	mmsg "net: waiting for ${1}"
+net_wait()
+{
+	step "net: waiting for ${1} ... "
 	for i in $(seq 1 50); do
 		if [ "$(ifconfig -a -s | grep ${1})" != "" ]; then
-			mmsg "net: ${1} found"
+			success
 			return
 		fi
 		sleep 0.2
 	done
-	emsg "net: timeout, skipping"
+	error
 }
 
-setup_network() {
-	ETH_IF="enp4s0"
-	WIFI_IF="wlo1"
-	NS="1.1.1.1"
+setup_network()
+{
+        ETH_IF="enp12s0"
+        WIFI_IF="wlo1"
+        NS="1.1.1.1"
 
-	sysctl -w net.ipv4.ping_group_range="0 1000" > /dev/null
+        mmsg "net: setup network"
 
-	net_wait "${ETH_IF}"
-	# ip addr add 192.168.0.4/24 dev ${ETH_IF}
-	# ip link set dev enp4s0 up
+        sysctl -w net.ipv4.ping_group_range="0 1000" > /dev/null
 
-	rfkill unblock wlan
-	net_wait "${WIFI_IF}"
+        # setup localhost
+        ip addr add 127.0.0.1/24 dev lo
+        ip link set dev lo up
 
-	mmsg "net: starting wpa_supplciant"
-	/usr/bin/wpa_supplicant -qq -Dnl80211 -i${WIFI_IF} -c/etc/wpa_supplicant.conf 2>&1 > /dev/null &
-	mmsg "net: starting dhclient"
-	dhclient "${WIFI_IF}" > /dev/null
+        net_wait "${ETH_IF}"
+        ip addr add 192.168.0.2/24 dev ${ETH_IF}
+        ip link set dev ${ETH_IF} up
 
-	# setup gateway
-	# route add default gw 192.168.0.1
+        # rfkill unblock wlan
+        # net_wait "${WIFI_IF}"
 
-	# setup dns
-	echo "nameserver ${NS}" > /etc/resolv.conf
+        # mmsg "net: starting wpa_supplciant"
+        # /usr/bin/wpa_supplicant -qq -Dnl80211 -i${WIFI_IF} -c/etc/wpa_supplicant.conf 2>&1 > /dev/null &
+        # mmsg "net: starting dhclient"
+        # dhclient "${WIFI_IF}" > /dev/null
+
+        # setup gateway
+        route add default gw 192.168.0.1
+
+        # setup dns
+        echo "nameserver ${NS}" > /etc/resolv.conf
 }
 
-setup_backlight
 setup_network
+
+# do anything else here
