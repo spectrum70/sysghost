1a18ba12e35ecfd8f34e209a7a6dc897847ab497
Angelo Dureghello <angelo@kernel-space.org>
2025-04-02 19:01:55 +0200

    scripts: add function to launch a service
    Signed-off-by: Angelo Dureghello <angelo@kernel-space.org>

--- scripts/lib.sh
+++ scripts/lib.sh
@@ -1,9 +1,33 @@
 # sysghost library
 
-mmsg() {
-	echo -e "\x1b[94m● \x1b[0m${1}"
+success()
+{
+	echo -e "\x1b[94msuccess\x1b[0m"
 }
 
-emsg() {
+error()
+{
+	echo -e "\x1b[94merror\x1b[0m"
+}
+
+step()
+{
+	echo -e -n "\x1b[94m● \x1b[0m${1}"
+}
+
+emsg()
+{
 	echo -e "\x1b[31;1m* ${1}\x1b[0m"
 }
+
+start_service()
+{
+	step "service: starting ${1} ... "
+	$@
+	if [ $? == 0 ]; then
+		success
+	else
+		error
+	fi
+}
+
