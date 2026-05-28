9e44c611ebc81aa55082588fbf45ae35d22e800b
Angelo Dureghello <angelo@kernel-space.org>
2026-05-28 22:34:09 +0200

    fix compilation issue related to bool
    New compiler does not like bool usage.
    
    Signed-off-by: Angelo Dureghello <angelo@kernel-space.org>

--- include/halt.h
+++ include/halt.h
@@ -1,8 +1,11 @@
 #ifndef halt_h
 #define halt_h
 
+#include <stdbool.h>
+
 int system_down(int reboot_system, bool sudo);
 int spawn(int silent, char *prog, ...);
 void close_all_streams(bool redirect_tty);
 void finalize_reboot(int type);
+
 #endif /* halt_h */
--- lib/halt.c
+++ lib/halt.c
@@ -22,6 +22,7 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
